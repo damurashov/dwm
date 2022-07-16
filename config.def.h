709fac3cffd6d1ce7df7d543f70180f99aa988e6
DmitryMurashov <dmtr.murashov@gmail.com>
2022-07-16 16:33:11 +0300

    [:dwm,config.def] Enh | Adjacenttagskipvacant

--- config.def.h
+++ config.def.h
@@ -90,6 +90,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,  viewnext,      {0} },
+	{ MODKEY|ShiftMask,             XK_j,   viewprev,      {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
